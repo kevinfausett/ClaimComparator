451e75fbeb6d1c8ce478cfdb79dff9281a8fc919
Andrew Costenoble <costenoble.a@husky.neu.edu>
2018-11-22 14:37:06 -0500

    Added a naive claim comparator that examines subject, verb, and object of two claims for synonyms

--- ClaimComparatorModel.py
+++ ClaimComparatorModel.py
@@ -1,16 +1,63 @@
 from SyntacticParser.SyntacticParser import SyntacticParser
 from LogicParser.LogicParser import LogicParser
+import nltk
+from nltk.corpus import wordnet
+
+nltk.download('wordnet')
 
 
 class ClaimComparatorModel:
     def __init__(self, text):
         self.syntax_parser = SyntacticParser(text)
         self.logic_parser = LogicParser(self.syntax_parser.go())
+        self.claims, self.parse_tree = self.logic_parser.go()
 
-    def go(self):
-        return self.logic_parser.go()
 
-def demo():
-    text = "Against this backdrop, a rise in violent crime has left some voters yearning for order and security, which Bolsonaro — an ex-military officer — promised to deliver. But his embrace of “law and order” carries alarming undertones, as he has expressed a fondness for the country’s past military dictatorship. His anti-democratic views are just one element of his disturbing rhetoric, though; the president-elect also freely spews misogynistic, anti-LGBTQ, and racist statements."
+    # Compares two claims to determine if they are equivalent
+    @staticmethod
+    def claims_equal(c1, c2):
+        subj1, verb1, obj1 = SyntacticParser.parts_of_sentence(c1)
+        subj1 = preprocess_string(subj1)
+        verb1 = preprocess_string(verb1)
+        obj1 = preprocess_string(obj1)
+
+        subj2, verb2, obj2 = SyntacticParser.parts_of_sentence(c2)
+        subj2 = preprocess_string(subj2)
+        verb2 = preprocess_string(verb2)
+        obj2 = preprocess_string(obj2)
+
+        return are_synonymous(subj1, subj2) and are_synonymous(verb1, verb2) and are_synonymous(obj1, obj2)
+
+
+# Strips a leading 'the ' from the string if it exists, and replaces all spaces with underscores
+def preprocess_string(string):
+    if string[:4] == 'the ':
+        return string[4:].replace(' ', '_')
+    else:
+        return string.replace(' ', '_')
+
+
+# Determines if the two given phrases are synonyms
+def are_synonymous(p1, p2):
+    p1_set = wordnet.synsets(p1)
+    p2_set = wordnet.synsets(p2)
+    p1_synonyms = []
+    for syn in p1_set:
+        for l in syn.lemmas():
+            p1_synonyms.append(l.name())
+    p2_synonyms = []
+    for syn in p2_set:
+        for l in syn.lemmas():
+            p2_synonyms.append(l.name())
+
+    are_synonyms = False
+    for sense in p1_synonyms:
+        if sense in p2_synonyms:
+            are_synonyms = True
+    return are_synonyms
+
+
+if __name__ == "__main__":
+    text = "The president won the election."
     ccm = ClaimComparatorModel(text)
-    print(ccm.go())
+    print(ClaimComparatorModel.claims_equal(ccm.parse_tree, ccm.parse_tree))
--- SyntacticParser/SyntacticParser.py
+++ SyntacticParser/SyntacticParser.py
@@ -9,6 +9,33 @@ class SyntacticParser:
         parser = Parser()
         return parser.parse(self.text)
 
+    # The subject of a sentence is defined as the first NP that is a child of an S and sibling of a VP
+    # The object of a sentence is defined as the first NP that is a child of a VP
+    # Returns the subject, main verb, and direct object of the sentence as strings or None, else returns None
+    # if it was not passed a sentence
+    @staticmethod
+    def parts_of_sentence(parse_tree):
+        if parse_tree.label()[0] == 'S':
+            subject = None
+            sibling_VP = False
+            verb = None
+            object = None
+            for phrase in parse_tree:
+                if phrase.label() == 'NP' and subject is None:
+                    subject = ' '.join(phrase.leaves())
+                elif phrase.label() == 'VP':
+                    sibling_VP = True
+                    for subphrase in phrase:
+                        if 'VB' in subphrase.label():
+                            verb = ' '.join(subphrase.leaves())
+                        elif subphrase.label() == 'NP':
+                            object = ' '.join(subphrase.leaves())
+            if not sibling_VP:
+                subject = None
+            return subject, verb, object
+        else:
+            return None
+
     def go(self):
         return self.syntactic_parse()
 
